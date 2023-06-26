1e73cebb8a29453389d4b10a108b489cde39adab
junyeong.jo <whwnsdud2024@naver.com>
2023-06-26 17:40:06 +0900

    Hello 서블릿

--- src/main/java/com/example/springmvc_1/SpringMvc1Application.java
+++ src/main/java/com/example/springmvc_1/SpringMvc1Application.java
@@ -2,12 +2,14 @@ package com.example.springmvc_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringMvc1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMvc1Application.class, args);
     }
-
 }
