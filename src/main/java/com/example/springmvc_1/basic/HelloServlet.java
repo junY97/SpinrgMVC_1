edddfdbf708ceb1ab52c05f99322b18d073048ca
junyeong.jo <whwnsdud2024@naver.com>
2023-06-27 09:34:45 +0900

    HttpServletRequest - 개요

--- src/main/java/com/example/springmvc_1/basic/HelloServlet.java
+++ src/main/java/com/example/springmvc_1/basic/HelloServlet.java
@@ -17,6 +17,12 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /*
+         * HttpServletRequest 역할
+         * HTTP 요청 메시지를 개발자가 직접 파싱해서 사용해도 되지만, 매우 불편할 것이다.
+         * 서블릿은 개발자가 HTTP 요청 메시지를 편리하게 사용할 수 있도록 개발자 대신에 HTTP 요청 메시지를 파싱한다.
+         * 그리고 그 결과를 HttpServletRequest 객체에 담아서 제공한다.
+         */
         System.out.println("HelloServlet.service");
         System.out.println("request = " + request);
         System.out.println("response = " + response);
