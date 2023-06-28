8e0a73af5f16a4729e03985cbe4ff7240a6fcb01
junyeong.jo <whwnsdud2024@naver.com>
2023-06-28 14:29:19 +0900

    HTTP 응답 데이터 - API JSON

---
+++ src/main/java/com/example/springmvc_1/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,38 @@
+package com.example.springmvc_1.basic.response;
+
+import com.example.springmvc_1.basic.HelloData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-06-28
+ */
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //Content-type: application/json
+        response.setContentType("application/json");
+        response.setStatus(200);
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        //{"username":"kim", "age":20}
+
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+    }
+}
