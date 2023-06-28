bca53b49599cea152452bf1b985641778c729e4f
junyeong.jo <whwnsdud2024@naver.com>
2023-06-28 10:26:31 +0900

    HTTP 요청 데이터 - API 메시지 바디 - 단순 텍스트

---
+++ src/main/java/com/example/springmvc_1/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,29 @@
+package com.example.springmvc_1.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-06-28
+ */
+
+@WebServlet(name = "requestBodyServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String message = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("message = " + message);
+        response.getWriter().println("ok");
+    }
+}
