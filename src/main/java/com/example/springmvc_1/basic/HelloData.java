efadbce81396bdf5ba1ebbf1b5d5b0f20f68a96a
junyeong.jo <whwnsdud2024@naver.com>
2023-06-28 10:54:49 +0900

    HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ src/main/java/com/example/springmvc_1/basic/HelloData.java
@@ -0,0 +1,16 @@
+package com.example.springmvc_1.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-06-28
+ */
+
+@Getter
+@Setter
+public class HelloData {
+    private String username;
+    private int age;
+}
---
+++ src/main/java/com/example/springmvc_1/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,34 @@
+package com.example.springmvc_1.basic.request;
+
+import com.example.springmvc_1.basic.HelloData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-06-28
+ */
+@WebServlet(name = "requestJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String message = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        HelloData helloData = objectMapper.readValue(message, HelloData.class);
+        System.out.println("helloData.username = " + helloData.getUsername());
+        System.out.println("helloData.age = " + helloData.getAge());
+    }
+}
