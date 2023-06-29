1ebdce921c1652e553ad03ca471ec9a809a122ab
junyeong.jo <whwnsdud2024@naver.com>
2023-06-29 14:06:16 +0900

    MVC 패턴 - 적용

---
+++ src/main/java/com/example/springmvc_1/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,37 @@
+package com.example.springmvc_1.web.servletmvc;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-06-29
+ */
+
+import com.example.springmvc_1.member.Member;
+import com.example.springmvc_1.member.MemberRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MvcMemberListServlet.service");
+
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
