66c33e478de4c69c96790dbe2a362597d55848e7
junyeong.jo <whwnsdud2024@naver.com>
2023-07-06 17:36:35 +0900

    핸들러 매핑과 핸들러 어댑터

---
+++ src/main/java/com/example/springmvc_1/web/springmvc/old/MyHttpRequestHandler.java
@@ -0,0 +1,22 @@
+package com.example.springmvc_1.web.springmvc.old;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+import java.io.IOException;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-07-06
+ */
+
+@Component("/springmvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler {
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MyHttpRequestHandler.handleRequest");
+    }
+}
---
+++ src/main/java/com/example/springmvc_1/web/springmvc/old/OldController.java
@@ -0,0 +1,21 @@
+package com.example.springmvc_1.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-07-06
+ */
+
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
