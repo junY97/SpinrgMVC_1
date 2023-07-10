4a4f790730b1897ce375f9528feea4f48fba44bb
junyeong.jo <whwnsdud2024@naver.com>
2023-07-10 11:05:32 +0900

    스프링 MVC - 컨트롤러 통합

---
+++ src/main/java/com/example/springmvc_1/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,56 @@
+package com.example.springmvc_1.web.springmvc.v2;
+
+import com.example.springmvc_1.member.Member;
+import com.example.springmvc_1.member.MemberRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-07-10
+ */
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+    @RequestMapping("/save")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+
+    @RequestMapping
+    public ModelAndView members() throws ServletException, IOException {
+        System.out.println("MvcMemberListServlet.service");
+
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+
+}
